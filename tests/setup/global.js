a7f87af8fbaadef46794f488d7520f65514ab29f
Artem Zakharchenko <kettanaito@gmail.com>
2026-07-03 15:50:13 +0200

    chore: formatting

--- tests/setup/global.js
+++ tests/setup/global.js
@@ -1,8 +1,8 @@
-import { fileURLToPath } from 'node:url';
-import { setupGlobal } from 'sv/testing';
+import { fileURLToPath } from 'node:url'
+import { setupGlobal } from 'sv/testing'
 
-const TEST_DIR = fileURLToPath(new URL('../../.test-output/', import.meta.url));
+const TEST_DIR = fileURLToPath(new URL('../../.test-output/', import.meta.url))
 
 export default setupGlobal({
-	TEST_DIR,
-});
+  TEST_DIR,
+})
